3d024891fe7008e6440dfd20500cc59341167b22
Bao Nguyen <b@nqbao.com>
2025-07-05 15:44:47 -0700

    add mapping from wiki page

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     ],
     python_requires=">=3.10",
     install_requires=[
-        # Add your dependencies here
+        "msgspec>=0.19.0",
     ],
     extras_require={
         "dev": [
